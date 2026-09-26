22ba7fab30c79894d115a1f4e7257c37f84c206d
Arsh <arshsingh1641@outlook.com>
2026-09-26 13:41:52 +0530

    add user routes for changing password, retrieving current user, updating account details, and fetching watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.model.js'
 import { deleteFromCloudinary, uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
 const generateAccessAndReferenceTokens = async (userId) => {
     try {
         const user = await User.findById(userId);
